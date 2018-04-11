d1f41b9193873ecd8804c25bd0ea82be7b97df8f
aG0aep6G <aG0aep6G@users.noreply.github.com>
2018-04-11 12:53:59 +0200

    fix UTF-8 handling in ByStatement.findEnd
    `tail.countUntil(';')` counted code points, but pos is used as a number of
    code units.

--- source/tests.d
+++ source/tests.d
@@ -813,3 +813,8 @@ unittest // CachedResults copies
     assert(data[0][0].as!string == "ABC");
 }
 
+unittest // UTF-8
+{
+    auto db = Database(":memory:");
+    db.run("SELECT '\u2019\u2019';");
+}
